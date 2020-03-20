b06df2bcf43a5deb89c02ec58c5ca5fba9866971
BuildTools <unconfigured@null.spigotmc.org>
2020-03-20 14:38:28 +0100

    good start

--- src/main/java/fr/cocoraid/prodigybank/commands/MainCMD.java
+++ src/main/java/fr/cocoraid/prodigybank/commands/MainCMD.java
@@ -5,20 +5,16 @@ import co.aikar.commands.annotation.*;
 import fr.cocoraid.prodigybank.ProdigyBank;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
-import net.citizensnpcs.api.trait.trait.Equipment;
-import net.citizensnpcs.trait.LookClose;
+import net.citizensnpcs.npc.skin.SkinnableEntity;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.mcmonkey.sentinel.SentinelTrait;
 
 @CommandAlias("prodigybank|pb")
 public class MainCMD extends BaseCommand {
 
-    private ProdigyBank instance;
+    private static ProdigyBank instance;
     public MainCMD(ProdigyBank instance) {
         this.instance = instance;
     }
@@ -28,41 +24,16 @@ public class MainCMD extends BaseCommand {
     public static void help(Player player, String[] args) {
         if (args.length == 0) {
             player.sendMessage("ceci est une aide");
+
+
             Location l = player.getLocation();
             NPC npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, "");
-            npc.addTrait(LookClose.class);
-            npc.getTrait(LookClose.class).setRange(2);
-            npc.getTrait(LookClose.class).setRandomLook(true);
-            npc.getTrait(LookClose.class).setRealisticLooking(true);
-            npc.getTrait(LookClose.class).toggle();
-            npc.addTrait(SentinelTrait.class);
-            npc.getTrait(Equipment.class)
-                    .set(Equipment.EquipmentSlot.HAND, new ItemStack(Material.DIAMOND_SWORD, 1));
+            npc.spawn(l);
+            ((SkinnableEntity) npc.getEntity()).setSkinPersistent("1f070ad7-9ee3-446a-a26a-8563146f711b",
+                    "hYsWnVE/XEPHDRjpMlblOoqGKI3jsUMBSBM9pG4Z91un7Y0TflPPyYVxBl3gnCL9dkO2iSEp4xuATMfFFUo3KvRaN47HUklL86KZSctDqej7CGzn3uGar+KP29uacyNZCuYFo98kwP8KHYaHLLCoEdoOUKOvFqZZkFJbRJbuhy2Fj+Kr47zfIDOiZfsacyAH2OQ0vbBg5+tRZi9lANniGUD6hc9zp660uOUu6B8yYBnUprCPnM2MUkV9lV9J7J7jd5oaOtj6rm5BDmYNwgWjLEJCdw5McqH6/KdMwj86fFs/2AGh8KVPXKzr6m7kMTJbpTFQDDKoo0aXXpHT/zAgAgq9Sx/sKn9ZN+hsJhDZqNvDV/h2GZe0bDyJ9t319mwkzMXLdN8QFYr3I+916eWmrztnc0Bj4drft+5EaEKTRDfgzrCHDVtguAUcOHk160qNApumzBfj6L3amOeiwmOwAktBZZCCiD+0Gz9MlVZ5eGHm2iruYi6SujIG6bIT+9/gzUnHo4mQSRzLBsKuYeWDLlNrwof6xleSM8WXV2RaUZDjJ7VOedkyzhEOa2ShP+D8CrYgwM+tv5nHzN/yajiOvQ1gPcx4UiTHMBXHzpUykjRyWDRQgEbpAdHwnrjq75C3QZlIsNr3KRtZ4CTfGKE+rJqFNufFHONoiX4kTNnn9YU=",
+                    "eyJ0aW1lc3RhbXAiOjE1ODMxMjIwMDA0MzQsInByb2ZpbGVJZCI6IjdkYTJhYjNhOTNjYTQ4ZWU4MzA0OGFmYzNiODBlNjhlIiwicHJvZmlsZU5hbWUiOiJHb2xkYXBmZWwiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2E3MDM0YzIzNDczNzhhOTBjMDkxMTNiYWM3ODI5YzkzYWIzMTBjNDNiOTI0N2ZiMDcxZWE0M2U5MTA2NGE0ZDkifX19");
 
-            SentinelTrait sentinel = npc.getTrait(SentinelTrait.class);
-            sentinel.fightback = true;
-            sentinel.realistic = false;
 
-            sentinel.speed = 1.2F;
-            npc.getNavigator().getLocalParameters().baseSpeed(1.2F);
-            sentinel.squad = "police";
-            sentinel.respawnTime = 0;
-            npc.data().setPersistent(NPC.RESPAWN_DELAY_METADATA, -1);
-
-            sentinel.attackRate = 4;
-            sentinel.range = 100;
-            sentinel.chaseRange = 100;
-            npc.getNavigator().getDefaultParameters().range((float) 100);
-            npc.data().setPersistent(NPC.PATHFINDER_OPEN_DOORS_METADATA,true);
-            npc.data().setPersistent("nameplate-visible", false);
-            npc.spawn(location);
-            police.add(npc);
-            player.sendMessage(
-                    new StringBuilder(lang.object_added).toString()
-                            .replace("%id",String.valueOf(npc.getId()))
-                            .replace("%type", "police member")
-            );
-            bankLoader.setPolicePos(npc);
             //ProdigyBank.getInstance().getBank().getHoldUp().endHoldUp();
 
         }
